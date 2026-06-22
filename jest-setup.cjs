5bceecc412cc23246a55ee5286a8c50fcfdeb112
pranavsubbareddy <pranavsubbareddy11@gmail.com>
2026-06-22 19:31:01 +0530

    Remove all test files, jest config, and test dependencies

--- jest-setup.cjs
+++
@@ -1,9 +0,0 @@
-// Polyfill TextEncoder/TextDecoder for LangChain in Jest (jsdom environment)
-const { TextEncoder, TextDecoder } = require('util');
-global.TextEncoder = TextEncoder;
-global.TextDecoder = TextDecoder;
-
-// Polyfill ReadableStream for @langchain/openai and @langchain/google-genai
-if (typeof global.ReadableStream === 'undefined') {
-  global.ReadableStream = require('stream/web').ReadableStream;
-}
